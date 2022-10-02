c20aadf2555c0603d067a89d8b6b83283471d479
karkrish2009 <karkrish2009@gmail.com>
2022-10-01 21:24:53 -0700

    Homework 4

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,42 +1,110 @@
 import Number from "./Number";
 import Operation from "./Operation";
 import Screen from "./Screen";
+import React, {useState} from 'react';
+import '../App.css';
 
 const Calculator = () => {
   /** TODO: Here is where you are going to keep track of calculator state */
 
+  // useState handle to keep track of the display value - number
+  const [number, setNumber] = useState("0");
+
+  // helper function to create the buttons for numbers
+  const createNumbers = () => {
+    const nbr = [];
+
+    for (let i = 0; i < 10; i++)
+    {
+      nbr.push(
+          <Number value={i.toString()} onClick={handleNumberClick} />
+      )
+    }
+
+    return nbr;
+
+
+  }
+
+  const opers = ['+', '-', '/', '*'];
+
   /** TODO: what happens when I click a number? */
-  const handleNumberClick = () => {};
+  const handleNumberClick = (value) => {
+    // Adds the userInput if number to the number
+    /*
+    console.log(event.target.innerHTML);
+    newNumber = event.target.innerHTML; */
+    //console.log("I am here with butoon click");
+    //console.log(typeof(value));
+    //console.log(Object.values(value)[0]);
+    //console.log(number);
+    let userInput = Object.values(value)[0];
+    //console.log("checking for zero", number.slice(0, -1))
+    if (number === '0'){
+      setNumber(userInput);
+    }
+    else {
+      setNumber(number + userInput);
+    }
+
+
+  };
 
   /** TODO: what happens when I click an operation? */
-  const handleOperationClick = () => {};
+  const handleOperationClick = (value) => {
+    // Does more of the hard work when adding an operator
+
+    //console.log("I am here with operator click");
+    //console.log(number);
+    let userInput = Object.values(value)[0];
+    //console.log("operas includes", opers.includes(number.slice(-1)));
+
+    // CASE 1: Operator is "=" - then evaluates and sets the Number
+    if (userInput === '=') {
+      setNumber(eval(number).toString());
+    }
+
+    // CASE 2: If Clear - set to zero
+    else if (userInput === 'clear') {
+      setNumber('0');
+      //console.log("after clear", number)
+    }
+
+    // Case 3: if preceding input is an operator return with no change (policy)
+    else if (number === '' || opers.includes(number.slice(-1))) {
+      return
+    }
+
+    // Case 4: None of the above - add the operator
+    else {
+      setNumber(number + userInput);
+    }
+
+  };
 
   return (
-    <div>
-      <Screen value="123" />
-      <div style={{ display: "flex" }}>
-        <div>
-          <Number value={0} onClick={handleNumberClick} />
-          <Number value={1} onClick={handleNumberClick} />
-          <Number value={2} onClick={handleNumberClick} />
-          <Number value={3} onClick={handleNumberClick} />
-          <Number value={4} onClick={handleNumberClick} />
-          <Number value={5} onClick={handleNumberClick} />
-          <Number value={6} onClick={handleNumberClick} />
-          <Number value={7} onClick={handleNumberClick} />
-          <Number value={8} onClick={handleNumberClick} />
-          <Number value={9} onClick={handleNumberClick} />
-        </div>
-        <div style={{ paddingLeft: 10 }}>
+    <div className = "calculator">
+
+
+      <Screen value={number} />
+
+
+      <div className = "numbers">
+        { createNumbers () }
+
+      </div>
+
+      
+        <div className = "operators">
           <Operation value="+" onClick={handleOperationClick} />
           <Operation value="/" onClick={handleOperationClick} />
-          <Operation value="x" onClick={handleOperationClick} />
+          <Operation value="*" onClick={handleOperationClick} />
           <Operation value="-" onClick={handleOperationClick} />
           <Operation value="=" onClick={handleOperationClick} />
           <Operation value="clear" onClick={handleOperationClick} />
         </div>
-      </div>
     </div>
+
   );
 };
 
--- src/components/Number.js
+++ src/components/Number.js
@@ -1,15 +1,39 @@
+import '../App.css';
+
 const Number = ({ value, onClick }) => {
   /** TODO: What happens when a user clicks a number, what do we want to pass to our parent? */
   return (
-    <div
+    <div>
+      <button
       style={{
-        padding: 10,
+          border: "1px solid black",
+          //padding: "16px",
+          //width: 300,
+          //height: 70,
+          textAlign: "center",
+          marginBottom: 10,
+          backgroundColor: "#131a26",
+          color: "#eee",
+          fontSize: "24px",
+          fontWeight: 300,
+          flexWrap: "wrap",
+          maxWidth: "33%",
+        }}
+        onClick = {() => onClick({value})}>
+      {value}
+      </button>
+    </div>
+    /*  style={{
+        padding: "16px",
         border: "1px solid black",
         width: 60,
-      }}
-    >
-      {value}
-    </div>
+        color: "#FFF",
+        cursor: "pointer",
+        transition: "0.4s"
+      }} */
+
+
+
   );
 };
 
